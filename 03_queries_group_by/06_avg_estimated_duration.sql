d34f2536044d3795e460216d8b0da9ce2ee2ac4a
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-10-08 18:22:44 +0000

    Add queries using group by and having more extesively

--- 03_queries_group_by/06_avg_estimated_duration.sql
+++ 03_queries_group_by/06_avg_estimated_duration.sql
@@ -2,11 +2,8 @@ SELECT students.name as student, AVG(assignment_submissions.duration) as average
 FROM students
 JOIN assignment_submissions ON student_id = students.id
 JOIN assignments ON assignment_id = assignments.id
-
-WHERE end_date IS NULL
+WHERE students.end_date IS NULL
 GROUP BY student
 HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
-
 ORDER BY average_estimated_duration 
-
 
